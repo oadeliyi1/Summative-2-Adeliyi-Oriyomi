6e99070ede47d26b73b582b6b8248b34d41e53fb
oadeliyi1 <oriyomiadeliyi@gmail.com>
2023-03-10 17:13:24 -0500

    Complete BookRepositoryTest

--- src/test/java/com/company/bookstore/repository/BookRepositoryTest.java
+++ src/test/java/com/company/bookstore/repository/BookRepositoryTest.java
@@ -10,7 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
+
+import static org.junit.Assert.*;
+
 
 
 public class BookRepositoryTest {
@@ -49,6 +53,9 @@ public class BookRepositoryTest {
         publisher.setEmail("publisher@penguin.com");
         publisher.setStreet("312 Main Road");
 
+        publisher = publisherRepository.save(publisher);
+
+
         Book book = new Book();
         book.setIsbn("0-1024-1680-X");
         book.setPrice(BigDecimal.valueOf(30));
@@ -57,9 +64,10 @@ public class BookRepositoryTest {
         book.setAuthorId(author.getId());
         book.setPublisherId(publisher.getId());
 
-        book1 = bookRepository.save(book1);
+        book = bookRepository.save(book);
 
-        Optional<Book> book
+        Optional<Book> book1 = bookRepository.findById(book.getId());
+        assertEquals(book1.get(),book);
 
 
 //
@@ -72,8 +80,168 @@ public class BookRepositoryTest {
 
 
     }
-    public void shouldUpdateBook(){}
-    public void shoulDeleteBook(){}
-    public void shouldFindBookbyID(){}
-    public void shouldReadAllBooks(){}
+    public void shouldUpdateBook(){
+        public void shouldAddBook(){
+
+            Author author = new Author();
+            author.setFirstName("Jonathan");
+            author.setLastName("Sanchez");
+            author.setStreet("123 Lane");
+            author.setCity("Miami");
+            author.setState("Florida");
+            author.setPostal_code("12345");
+            author.setPhone("111-222-3456");
+            author.setEmail("jon@gmail.com");
+
+            author = authorRepository.save(author);
+
+            Publisher publisher = new Publisher();
+            publisher.setCity("Detroit");
+            publisher.setState("Michigan");
+            publisher.setPhone("800-345-1345");
+            publisher.setPostal_code("30456");
+            publisher.setEmail("publisher@penguin.com");
+            publisher.setStreet("312 Main Road");
+            publisher = publisherRepository.save(publisher);
+
+            Book book = new Book();
+            book.setIsbn("0-1024-1680-X");
+            book.setPrice(BigDecimal.valueOf(30));
+            book.setTitle("Moby Dick");
+            book.setPublishDate("Jan-01-1899");
+            book.setAuthorId(author.getId());
+            book.setPublisherId(publisher.getId());
+
+            book = bookRepository.save(book);
+
+            book.setPublishDate("Feb-02-1900");
+
+            book = bookRepository.save(book);
+
+            Optional<Book> book1 = bookRepository.findById(book.getId());
+            assertEquals(book1.get(),book);
+    }
+    public void shoulDeleteBook(){            Author author = new Author();
+            author.setFirstName("Jonathan");
+            author.setLastName("Sanchez");
+            author.setStreet("123 Lane");
+            author.setCity("Miami");
+            author.setState("Florida");
+            author.setPostal_code("12345");
+            author.setPhone("111-222-3456");
+            author.setEmail("jon@gmail.com");
+
+            author = authorRepository.save(author);
+
+            Publisher publisher = new Publisher();
+            publisher.setCity("Detroit");
+            publisher.setState("Michigan");
+            publisher.setPhone("800-345-1345");
+            publisher.setPostal_code("30456");
+            publisher.setEmail("publisher@penguin.com");
+            publisher.setStreet("312 Main Road");
+
+            publisher = publisherRepository.save(publisher);
+
+            Book book = new Book();
+            book.setIsbn("0-1024-1680-X");
+            book.setPrice(BigDecimal.valueOf(30));
+            book.setTitle("Moby Dick");
+            book.setPublishDate("Jan-01-1899");
+            book.setAuthorId(author.getId());
+            book.setPublisherId(publisher.getId());
+
+            book = bookRepository.save(book);
+
+             bookRepository.deleteById(book.getId());
+
+            Optional<Book> fromRepo = bookRepository.findById(book.getId());
+            assertFalse(fromRepo.isPresent());
+        }
+    public void shouldFindBookbyID(){
+        Author author = new Author();
+            author.setFirstName("Jonathan");
+            author.setLastName("Sanchez");
+            author.setStreet("123 Lane");
+            author.setCity("Miami");
+            author.setState("Florida");
+            author.setPostal_code("12345");
+            author.setPhone("111-222-3456");
+            author.setEmail("jon@gmail.com");
+
+            author = authorRepository.save(author);
+
+            Publisher publisher = new Publisher();
+            publisher.setCity("Detroit");
+            publisher.setState("Michigan");
+            publisher.setPhone("800-345-1345");
+            publisher.setPostal_code("30456");
+            publisher.setEmail("publisher@penguin.com");
+            publisher.setStreet("312 Main Road");
+
+            publisher = publisherRepository.save(publisher);
+
+            Book book = new Book();
+            book.setIsbn("0-1024-1680-X");
+            book.setPrice(BigDecimal.valueOf(30));
+            book.setTitle("Moby Dick");
+            book.setPublishDate("Jan-01-1899");
+            book.setAuthorId(author.getId());
+            book.setPublisherId(publisher.getId());
+
+            book = bookRepository.save(book);
+
+            Optional<Book> book1 = bookRepository.findById(book.getId());
+            assertEquals(book1.get(),book);
+    }
+    public void shouldReadAllBooks(){
+            Author author = new Author();
+            author.setFirstName("Jonathan");
+            author.setLastName("Sanchez");
+            author.setStreet("123 Lane");
+            author.setCity("Miami");
+            author.setState("Florida");
+            author.setPostal_code("12345");
+            author.setPhone("111-222-3456");
+            author.setEmail("jon@gmail.com");
+
+            author = authorRepository.save(author);
+
+            Publisher publisher = new Publisher();
+            publisher.setCity("Detroit");
+            publisher.setState("Michigan");
+            publisher.setPhone("800-345-1345");
+            publisher.setPostal_code("30456");
+            publisher.setEmail("publisher@penguin.com");
+            publisher.setStreet("312 Main Road");
+
+            publisher = publisherRepository.save(publisher);
+
+            Book book = new Book();
+            book.setIsbn("0-1024-1680-X");
+            book.setPrice(BigDecimal.valueOf(30));
+            book.setTitle("Moby Dick");
+            book.setPublishDate("Jan-01-1899");
+            book.setAuthorId(author.getId());
+            book.setPublisherId(publisher.getId());
+
+            book = bookRepository.save(book);
+
+            Book book2 = new Book();
+            book.setIsbn("0-1134-1680-X");
+            book.setPrice(BigDecimal.valueOf(20));
+            book.setTitle("Pride & Prejudice");
+            book.setPublishDate("Jan-02-1899");
+            book.setAuthorId(author.getId());
+            book.setPublisherId(publisher.getId());
+            book2 = bookRepository.save(book2);
+
+            List<Book> bookList = bookRepository.findAll();
+            assertEquals(bookList.size(), 2);
+
+
+
+
+        }
+    }
 }
